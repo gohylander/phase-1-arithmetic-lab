14568a024914855761fa5fb1fd5a5e54a4843a23
George Hyland <gohylander@gmail.com>
2023-04-02 20:59:34 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+{let num1 = 31
+let num2 = 2
+let multiply = num1 * num2}
 
-// Write your code here
+{let num3 = 16
+ let num4 = 4
+let mod = 16/4}
+
+{let random = 
+Math.floor(Math.random() * 20) + 1;}
+
+{let max =
+    Math.max(2, 4, 6, 8, 10, 16, 20);}
